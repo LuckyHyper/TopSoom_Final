ea1655294e7f76d8683717e7f9d40758023b4d08
Hechmi BenHadjali <medbha55@yahoo.fr>
2022-02-23 10:38:12 +0100

    react setup with laravel done

---
+++ resources/js/components/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App1 from '../App1';
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App1 />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+export default Index;
+
